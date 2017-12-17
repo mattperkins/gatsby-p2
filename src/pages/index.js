b202038b354fde4f0a076335dccfd9413e581ec3
mattperkins <github@mattperkins.com>
2017-12-17 20:44:59 +0000

    init

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from './Header'
+import Footer from './Footer'
+
+export default () => 
+<div>
+  <Header masthead="This is the masthead"/>
+  <Footer footer="This is the footer"/>
+</div>
+
